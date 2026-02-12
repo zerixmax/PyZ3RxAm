92fac1dbb9617c47a431183c9590c1b85e0ba42b
zerixmax <zerix.m@gmail.com>
2026-02-12 14:54:05 +0100

    feat: configure static export for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export', // Omogućuje generiranje statičkih HTML/CSS/JS datoteka
+  images: {
+    unoptimized: true, // Potrebno za statički izvoz slika
+  },
 };
 
 export default nextConfig;
